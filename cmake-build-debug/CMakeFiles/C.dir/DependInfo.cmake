ef0abff15cb4140ed5672e45df9398b5faea5da1
Mahdi Haidary <m.mahdi.haidary.786@gmail.com>
2025-12-21 01:03:58 +0100

    fahr to celcius

---
+++ cmake-build-debug/CMakeFiles/C.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Byte Boss/Documents/Github/mmahdihaidary/C/Tutorials/Day 1/main.c" "CMakeFiles/C.dir/Tutorials/Day_1/main.c.obj" "gcc" "CMakeFiles/C.dir/Tutorials/Day_1/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
